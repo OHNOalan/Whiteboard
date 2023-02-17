4e089f73751e8041a54d1d638eff66c19d1a7882
Seth Hammell <shammell@uwaterloo.ca>
2023-02-16 22:35:11 -0500

    Resizable window

--- application/src/main/kotlin/net/codebot/application/components/AppCanvas.kt
+++ application/src/main/kotlin/net/codebot/application/components/AppCanvas.kt
@@ -1,6 +1,5 @@
 package net.codebot.application.components
 
-import net.codebot.application.components.tools.Text
 import javafx.event.EventHandler
 import javafx.geometry.Insets
 import javafx.scene.ImageCursor
@@ -8,19 +7,14 @@ import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.control.ColorPicker
 import javafx.scene.control.ScrollPane
-import javafx.scene.control.TextArea
 import javafx.scene.image.Image
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
-import javafx.scene.shape.Rectangle
 import net.codebot.application.components.tools.BaseTool
+import net.codebot.application.components.tools.Text
 
-class AppCanvas(borderPane: BorderPane) {
-    private var width = 800.0
-    private var height = 800.0
-
-    private val canvas = Canvas(width, height)
+class AppCanvas(borderPane: BorderPane): Canvas(800.0, 800.0) {
     private val texts: MutableList<Text> = mutableListOf()
     private val tools: MutableList<BaseTool> = mutableListOf()
     private var selectedTool: Int = 0
@@ -28,26 +22,24 @@ class AppCanvas(borderPane: BorderPane) {
     val pane: Pane = Pane()
 
     var backgroundColour = ColorPicker(Color.WHITE)
-    val context: GraphicsContext = canvas.graphicsContext2D
+    val context: GraphicsContext = this.graphicsContext2D
 
     init {
         pane.background = Background(BackgroundFill(backgroundColour.value, CornerRadii.EMPTY, Insets.EMPTY))
 
-        canvas.onMousePressed = EventHandler { e: MouseEvent ->
+        this.onMousePressed = EventHandler { e: MouseEvent ->
             tools[selectedTool].canvasMousePressed(e, context, pane)
         }
-        canvas.onMouseDragged = EventHandler { e: MouseEvent ->
+        this.onMouseDragged = EventHandler { e: MouseEvent ->
             tools[selectedTool].canvasMouseDragged(e, context, pane)
         }
-        canvas.onMouseReleased = EventHandler { e: MouseEvent ->
+        this.onMouseReleased = EventHandler { e: MouseEvent ->
             tools[selectedTool].canvasMouseReleased(e, context, pane)
         }
-        canvas.onMouseMoved = EventHandler { e: MouseEvent ->
+        this.onMouseMoved = EventHandler { e: MouseEvent ->
             tools[selectedTool].canvasMouseMoved(e, context, pane)
         }
-        canvas.widthProperty().bind(scrollPane.widthProperty())
-        canvas.heightProperty().bind(scrollPane.heightProperty())
-        pane.children.add(canvas)
+        pane.children.add(this)
         scrollPane.content = pane
         borderPane.center = scrollPane
     }
@@ -55,7 +47,7 @@ class AppCanvas(borderPane: BorderPane) {
     fun setTool(toolIndex: Int, cursorImage: Image) {
         tools[selectedTool].deselectTool()
         selectedTool = toolIndex
-        canvas.cursor = ImageCursor(
+        this.cursor = ImageCursor(
             cursorImage,
             0.0,
             cursorImage.height
@@ -68,6 +60,6 @@ class AppCanvas(borderPane: BorderPane) {
     }
 
     fun clearCanvas() {
-        context.clearRect(0.0, 0.0, canvas.width, canvas.height)
+        context.clearRect(0.0, 0.0, this.width, this.height)
     }
 }
